9ff6954def07dedbe838d83112f144de5f266c18
alaindemain <alain.nshimirimana@gmail.com>
2022-03-21 15:07:39 -0700

    I innitialized a connection string and Created an intro to the habit tracker app

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SQLite;
 
 namespace HabitTracker
 {
@@ -6,7 +7,54 @@ namespace HabitTracker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string connectionString = @"Data Source=habitTracker.db";
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                var cmd = connection.CreateCommand(); // create a command to send to the database
+                // create the database and table
+                cmd.CommandText = @"CREATE TABLE IF NOT EXISTS WorkoutTracker (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Date TEXT,
+                    Duration_hrs INTEGER
+                    )";
+                cmd.ExecuteNonQuery(); //execute the command w/ returning any values
+                connection.Close(); // close the connection
+            }
+            //intro to the app and get user input
+            Console.WriteLine("Hello! Welcome to the WorkoutTacker app where you will be able to log your workout.");
+            UserInput();
+        }
+        
+        static void UserInput()
+        {
+            Console.Clear();
+            bool closeApp = false;
+            int input;
+            while (closeApp == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("\nWhat would you like to do (TYPE A NUMBER)");
+                Console.ResetColor();
+                Console.WriteLine("0. Close app");
+                Console.WriteLine("1. View All Records");
+                Console.WriteLine("2. New Record Entry");
+                Console.WriteLine("3. Delete Record");
+                Console.WriteLine("4. Update Record");
+                Console.Write("> ");
+                do
+                {
+                    input = Console.Read();
+                }while (input < 0 || input > 4);
+
+                switch (input)
+                {
+                    case 0:
+                        Console.WriteLine("\nGoodbye");
+                        closeApp = true;
+                        break;
+                }
+            }
         }
     }
 }
